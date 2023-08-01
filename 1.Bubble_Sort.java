350f02e409d54326d06e55d163e1885be4a90374
Vishal Gupta <90566964+guptav100@users.noreply.github.com>
2023-08-01 23:01:12 +0530

    Add files via upload

---
+++ 1.Bubble_Sort.java
@@ -0,0 +1,25 @@
+
+class main {
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 8, 3, 9, 6, 2, 1 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // swap
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j] = temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
